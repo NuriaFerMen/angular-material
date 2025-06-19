5d63e9017e7512b5a8eb4b327843414426508132
NuriaFerMen <nukas27@gmail.com>
2025-06-19 12:39:04 +0200

    Fin de seccion 16

---
+++ src/app/auth/guards/public.guard.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, GuardResult, MaybeAsync, Route, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { AuthService } from '../services/auth-services';
+import { map, tap } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class publicGuard implements CanMatch, CanActivate{
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
+
+  }
+
+  private checkAuthStatus(): MaybeAsync<GuardResult>{
+    return this.authService.checkAuthentication()
+      .pipe(
+        map(isAuthenticated => {
+          if (!isAuthenticated) {
+            return true;
+          } else {
+            return this.router.createUrlTree(['./']);
+          }
+        })
+      );
+  }
+
+  canMatch(route: Route, segments: UrlSegment[]): MaybeAsync<GuardResult> {
+    // console.log('Can Match')
+    // console.log(route, segments);
+
+    return this.checkAuthStatus();
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
+    // console.log('Can activate')
+    // console.log(route, state);
+
+    return this.checkAuthStatus();
+  }
+}
